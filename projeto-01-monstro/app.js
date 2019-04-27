9eca9c6686d8b450740a9e0696a788e7f0806d09
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2019-04-27 16:34:26 -0300

    Implementando Atacar

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -15,9 +15,25 @@ new Vue({
             this.running = true
             this.playerLife = 100
             this.monsterLife = 100
+        },
+        attack(special) {
+            this.hurt('playerLife', 5, 10, false)
+            this.hurt('monsterLife', 7, 12, special)
+        },
+        hurt(atr, min, max, special){
+            const plus = special ? 5 : 0
+            const hurt = this.getRandom(min+plus, max+plus)
+            this[atr] = Math.max(this[atr] - hurt, 0) 
+            // garantir que sempre seja zero caso player life seja negativo
+        },
+        getRandom(min, max) {
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
         }
     },
     watch: {
-        
+        hasResult(value){
+            if(value) this.running = false
+        }
     },
 })
