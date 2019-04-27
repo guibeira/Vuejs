783494c6d1de89d63f5d14b3ad764258863381aa
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2019-04-27 16:41:30 -0300

    Implementando Curar

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -17,8 +17,8 @@ new Vue({
             this.monsterLife = 100
         },
         attack(special) {
-            this.hurt('playerLife', 5, 10, false)
-            this.hurt('monsterLife', 7, 12, special)
+            this.hurt('playerLife', 7, 12, false)
+            this.hurt('monsterLife', 5, 10, special)
         },
         hurt(atr, min, max, special){
             const plus = special ? 5 : 0
@@ -29,6 +29,14 @@ new Vue({
         getRandom(min, max) {
             const value = Math.random() * (max - min) + min
             return Math.round(value)
+        },
+        healAndHurt(){
+            this.heal(10, 15)
+            this.hurt('playerLife', 7, 12, false)
+        },
+        heal(min, max){
+            const heal = this.getRandom(min, max)
+            this.playerLife = Math.min(this.playerLife+ heal , 100)
         }
     },
     watch: {
