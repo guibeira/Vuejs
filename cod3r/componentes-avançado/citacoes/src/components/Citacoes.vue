ed0c3f59a2f411f0d331bb37c88e61549919fd80
guilhermevbeira <guilherme.vieira.beira@gmail.com>
2019-06-06 20:47:08 -0300

    Usando Múltiplos Slots (Slots Nomeados

--- cod3r/componentes-avançado/citacoes/src/components/Citacoes.vue
+++ cod3r/componentes-avançado/citacoes/src/components/Citacoes.vue
@@ -5,9 +5,9 @@
             <button @click="numero++">&gt;</button>
         </span>
         <citacao>
-            <h1>{{citacoes[indice].autor}}</h1>
-            <p>{{citacoes[indice].texto}}</p>
-            <h6>{{citacoes[indice].fonte}}</h6>
+            <h6 slot="fonte">{{citacoes[indice].fonte}}</h6>
+            <p slot="texto">{{citacoes[indice].texto}}</p>
+            <h1 slot="autor">{{citacoes[indice].autor}}</h1>
         </citacao>
     </div>
 </template>
