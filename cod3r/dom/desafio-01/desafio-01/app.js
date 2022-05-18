c7a5a25477a925336862a1d110532219305ba02a
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2022-05-18 14:27:37 -0300

    Pin vue version

--- cod3r/dom/desafio-01/desafio-01/app.js
+++ cod3r/dom/desafio-01/desafio-01/app.js
@@ -1,13 +1,13 @@
 new Vue({
-    el: '#desafio',
-    data: {
-        nome: 'Doggao',
-        idade: 28,
-        imgUrl: 'https://i.etsystatic.com/6945515/r/il/74e64b/1584597616/il_794xN.1584597616_7spu.jpg',
-    },
-    methods: {
-        randomNumber(){
-            return  Math.random().toString()
-        }
+  el: '#desafio',
+  data: {
+    nome: 'Doggao',
+    idade: 28,
+    imgUrl: 'https://i.etsystatic.com/6945515/r/il/74e64b/1584597616/il_794xN.1584597616_7spu.jpg',
+  },
+  methods: {
+    randomNumber() {
+      return Math.random().toString()
     }
+  }
 })
